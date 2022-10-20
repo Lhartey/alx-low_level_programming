e503abce1d81a9b5bb2b785f10593f25807ab2f0
Augustine Lartey <augustinelartey68@gmail.com>
2022-10-20 13:16:08 -0700

    Task fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,21 +2,18 @@
 
 /**
  * more_numbers - prints 0 to 14 ten times
- * Return: Always 0
  */
 
 void more_numbers(void)
 {
-	int num, c;
+	int num, count;
 
-	for (c = 0; c <= 9; c++)
+	for (count = 0; count <= 9; count++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
 			if (num > 9)
-			{
 				_putchar((num / 10) + '0');
-			}
 			_putchar((num % 10) + '0');
 		}
 		_putchar('\n');
