74589e1544adfec77cb7351b95cdaeb911a5e85d
Augustine Lartey <augustinelartey68@gmail.com>
2022-10-20 13:04:01 -0700

    Task 0-101

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints the numbers 0-9
+ * without numbers 2 and 4
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		if (a != 2 && a != 4)
+			_putchar(a + '0');
+
+		a++;
+	}
+	_putchar('\n');
+}
