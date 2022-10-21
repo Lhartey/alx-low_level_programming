5fa1c6bcf1e7d48e92f15c1b62eae50d61080a83
Augustine Lartey <augustinelartey68@gmail.com>
2022-10-20 18:52:40 -0700

    Task fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * _isupper - checks if a character is uppercase
@@ -8,8 +8,11 @@
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
-
+	}
 	else
+	{
 		return (0);
+	}
 }
