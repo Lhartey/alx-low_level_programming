13383dde3839916bca60078ff5af241a8676e710
Augustine Lartey <augustinelartey68@gmail.com>
2022-10-31 15:49:20 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,7 +7,7 @@
  * @n: The number of bytes to copy
  * Return: A pointer to the destination buffer @dest
  */
-void *_memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
 
