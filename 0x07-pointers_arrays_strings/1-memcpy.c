f61e2250709f7005567c4c0bb531c65ee7a015b8
Augustine Lartey <augustinelartey68@gmail.com>
2022-11-01 08:59:22 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,12 +9,14 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
+	char *ptr = dest;
 
-	for (index = 0; index < n; index++)
-		destination[index] = source[index];
+	while (n-- > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++
+	}
 
 	return (dest);
 }
