d3c125509e712cf02e5e363b3fc4ccfb6f855e68
Augustine Lartey <augustinelartey68@gmail.com>
2022-11-04 17:52:40 -0700

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,59 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 /**
- * main - Prints the addition of positive numbers
- * @argc: The number of arguments
- * @argv: An array of pointers
- * Return: If one of the numbers contains symbols that are non-digits - 1 Otherwise - 0
+ * isNum - check if string array is num
+ * @num: string to check
+ * Return: 0 if it's a number
+ * 1 if it's not a number
+ */
+int isNum(char num[])
+{
+	int i, l = strlen(num);
+
+	for (i = 0; i < l; i++)
+	{
+		if (!isdigit(num[i]))
+			return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * main - a program that adds positive numbers
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int num, digit, sum = 0;
+	int i, sum;
 
-	for (num = 1; num < argc; num++)
+	if (argc == 1)
 	{
-		for (digit = 0; argv[num][digit]; digit++)
+		printf("0\n");
+	}
+	else
+	{
+		sum = 0;
+		for (i = 1; i < argc; i++)
 		{
-			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			if (isNum(argv[i]) == 0)
+			{
+				sum += atoi(argv[i]);
+			}
+			else
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
 
-		sum += atoi(argv[num]);
+		printf("%d\n", sum);
 	}
-
-	printf("%d\n", sum);
 
 	return (0);
 }
